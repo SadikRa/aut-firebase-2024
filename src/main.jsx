67351c435c68c6622d686158765edf2a6f59f029
SadikRa <sadikrahman494@gmail.com>
2024-09-23 20:48:24 +0600

    reset project done

--- src/main.jsx
+++ src/main.jsx
@@ -1,37 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Main from './components/Main/Main';
-import Home from './components/Home/Home';
-import Login from './components/Login/Login';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Main />,
-    children: [
-        {
-          path: '/',
-          element: <Home />
-        },
-        {
-          path: '/login',
-          element: <Login></Login>
-        }
-    ]
-  },
-]);
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div className='container mx-auto'>
-    <RouterProvider router={router} />
-    </div>
+    <App />
   </StrictMode>,
 )
