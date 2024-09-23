8855811445368d4d2bfdd8b3c759672476dbd428
SadikRa <sadikrahman494@gmail.com>
2024-09-23 20:19:14 +0600

    main page added done

--- src/main.jsx
+++ src/main.jsx
@@ -6,17 +6,32 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Main from './components/Main/Main';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Main />,
+    children: [
+        {
+          path: '/',
+          element: <Home />
+        },
+        {
+          path: '/login',
+          element: <Login></Login>
+        }
+    ]
   },
 ]);
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <div className='container mx-auto'>
     <RouterProvider router={router} />
+    </div>
   </StrictMode>,
 )
